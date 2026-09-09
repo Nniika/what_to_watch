c2b979556519aa7901cd29dc7c06b943c86d8aa6
Nika <LA.N21@yandex.ru>
2026-09-09 18:43:42 +0300

    aaaaaaaaaaaaaa

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
