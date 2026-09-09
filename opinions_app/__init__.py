162141b4daa7d6e87c13c83995bd1c5ee2d5350a
Nika <LA.N21@yandex.ru>
2026-09-09 15:17:41 +0300

    Руфакторинг проекта: разбивка на пакеты

---
+++ opinions_app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object('settings.Config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from . import views, models, error_handlers, cli_commands
---
+++ opinions_app/views.py
@@ -0,0 +1,36 @@
+from random import randrange
+from flask import abort, redirect, render_template, url_for
+from . import app, db
+from .models import Opinion
+from .forms import OpinionForm
+
+
+@app.route('/')
+def index_view():
+    quantity = Opinion.query.count()
+    if not quantity:
+        abort(500)
+    offset_value = randrange(quantity)
+    opinion = Opinion.query.offset(offset_value).first()
+    return render_template('opinion.html', opinion=opinion)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_opinion_view():
+    form = OpinionForm()
+    if form.validate_on_submit():
+        opinion = Opinion(
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        db.session.add(opinion)
+        db.session.commit()
+        return redirect(url_for('opinion_view', id=opinion.id))
+    return render_template('add_opinion.html', form=form)
+
+
+@app.route('/opinions/<int:id>')
+def opinion_view(id):
+    opinion = Opinion.query.get_or_404(id)
+    return render_template('opinion.html', opinion=opinion)
