8f2fd0d6f6c1df061fced9493e62046e97e889bd
Nika <LA.N21@yandex.ru>
2026-09-09 16:31:58 +0300

    Исправлены ошибки PEP8 в пакете opinions_app

--- settings.py
+++ settings.py
@@ -4,3 +4,4 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
